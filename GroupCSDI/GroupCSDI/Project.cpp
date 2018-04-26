acb5054058be878b3ef833c401b63b07d112d47d
JakeWestcott <32679007+JakeWestcott@users.noreply.github.com>
2018-04-26 17:17:12 +0100

    Created the project class.

---
+++ GroupCSDI/GroupCSDI/Project.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "Project.h"
+//Here we will give the defintions for all the functions delcared in the header file
+//Add a project
+//Validate a project (this function will check to see if it exists)
+//Update a project
+//Delete a project
+
+Project::Project()
+{
+}
+
+
+Project::~Project()
+{
+}
---
+++ GroupCSDI/GroupCSDI/Project.h
@@ -0,0 +1,13 @@
+#pragma once
+class Project
+{
+public:
+	//Here we need the function declarations for the following functions:
+	//Add a project
+	//Validate a project (this function will check to see if it exists)
+	//Update a project
+	//Delete a project
+	Project();
+	~Project();
+};
+
