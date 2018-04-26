b1049eea7178854a45fd07109ef9103c36e30163
zakbowring <zbowring@sky.com>
2018-04-26 16:16:09 +0100

    update

--- GroupCSDI/GroupCSDI/EntryForm.cpp
+++ GroupCSDI/GroupCSDI/EntryForm.cpp
@@ -9,11 +9,6 @@ void Main(array<String^>^ args) {
 	Application::SetCompatibleTextRenderingDefault(false);
 	GroupCSDI::EntryForm form;
 	Application::Run(%form);
-<<<<<<< HEAD
-
-
-=======
 	//This is a GitHub Repo Test....Ignore this comment.
 	//testing
->>>>>>> af1352b91e68273d5e7bc88974d49788edff24be
 }
