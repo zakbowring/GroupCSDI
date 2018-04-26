4837a39e5211e4b57ffdae9d90fd782233f22289
zakbowring <zbowring@sky.com>
2018-04-26 17:46:11 +0100

    Moved the main into CinemaBaseX.

--- GroupCSDI/GroupCSDI/CinemaBaseX.cpp
+++ GroupCSDI/GroupCSDI/CinemaBaseX.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 #include "CinemaBaseX.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
 //Contructor
 CinemaBaseX::CinemaBaseX()
@@ -13,5 +16,20 @@ CinemaBaseX::~CinemaBaseX()
 }
 
 bool CinemaBaseX::Run() {
+	
+
+	cout << "-------------Cinema Base X-------------" << endl;
+	cout << "---------By Trek Star Pictures---------\n" << endl;
+	cout << "To enter the program type 'enter', to close the program type 'exit'" << endl;
+	string input;
+	cin >> input;
+	if (input == "enter")
+		cout << "entered the program" << endl;
+	else if (input == "exit")
+		cout << "Exit the program" << endl;
+	else
+		cout << "Input invalid" << endl;
+	cin >> input;
+	return false;
 
 }
